700e943eae2f2f3f419d1f21d8cdd0baec291837
Joanna Estrada <jest1617@students.vsu.edu>
2025-11-28 00:48:46 -0500

    lab 5

--- app/src/main/java/com/codepath/campgrounds/MainActivity.kt
+++ app/src/main/java/com/codepath/campgrounds/MainActivity.kt
@@ -2,6 +2,7 @@ package com.codepath.campgrounds
 
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -9,6 +10,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.codepath.campgrounds.databinding.ActivityMainBinding
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
@@ -41,7 +44,23 @@ class MainActivity : AppCompatActivity() {
         campgroundsRecyclerView.adapter = campgroundAdapter
 
         // TODO: Step 7: Load new items from our database
-
+        lifecycleScope.launch {
+            (application as CampgroundApplication).db.campgroundDao().getAll().collect { databaseList ->
+                databaseList.map { entity ->
+                    val images = listOf(CampgroundImage(url = entity.imageUrl, title = ""))
+                    Campground(
+                        entity.name,
+                        entity.description,
+                        entity.latLong,
+                        images
+                    )
+                }.also { mappedList ->
+                    campgrounds.clear()
+                    campgrounds.addAll(mappedList)
+                    campgroundAdapter.notifyDataSetChanged()
+                }
+            }
+        }
 
         campgroundsRecyclerView.layoutManager = LinearLayoutManager(this).also {
             val dividerItemDecoration = DividerItemDecoration(this, it.orientation)
@@ -68,9 +87,17 @@ class MainActivity : AppCompatActivity() {
                     )
 
                     parsedJson.data?.let { list ->
-                        campgrounds.addAll(list)
-
-                        campgroundAdapter.notifyDataSetChanged()
+                        lifecycleScope.launch(IO) {
+                            (application as CampgroundApplication).db.campgroundDao().deleteAll()
+                            (application as CampgroundApplication).db.campgroundDao().insertAll(list.map {
+                                CampgroundEntity(
+                                    name = it.name,
+                                    description = it.description,
+                                    latLong = it.latLong,
+                                    imageUrl = it.imageUrl
+                                )
+                            })
+                        }
                     }
 
                 } catch (e: JSONException) {
@@ -80,4 +107,4 @@ class MainActivity : AppCompatActivity() {
 
         })
     }
-}
+}//////////
